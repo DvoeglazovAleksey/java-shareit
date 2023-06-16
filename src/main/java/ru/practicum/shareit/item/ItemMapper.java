80543226bb9d514b2435c140e2472c425f9204bb
Aleksey_Dvoeglazov Java_Developer <alekdvoeglazov@yandex.ru>
2023-06-16 14:41:41 +0300

    fix: Class Mapper in UtilityClass.

--- src/main/java/ru/practicum/shareit/item/ItemMapper.java
+++ src/main/java/ru/practicum/shareit/item/ItemMapper.java
@@ -1,9 +1,11 @@
 package ru.practicum.shareit.item;
 
+import lombok.experimental.UtilityClass;
 import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.request.ItemRequest;
 
+@UtilityClass
 public class ItemMapper {
     public static ItemDto toItemDto(Item item) {
         return ItemDto.builder()
